6ae22093171da437c2d64387c80b9b7910993a44
Tilak <tilakagrawal5803@gmail.com>
2022-10-01 12:30:31 +0530

    All Set

---
+++ javascript/cards.js
@@ -0,0 +1,28 @@
+function places(car1, carryOne){
+    for(i=0; i<carryOne.length; i++){
+        jup = carryOne[i];
+        viewImg = allData[jup]['Media'][0];
+        Pname = name(jup);
+        spl = viewImg.split('.');
+        ftype = spl[spl.length-1]
+        if(ftype == "mp4"){
+            newA = `<a href="/product.html?name=`+Pname+"&jup="+jup+`" class="cardself">
+                <video src="/products/`+jup+`/`+viewImg+`" class="cardIMG"></video>
+                <div class="contentCard">
+                    <div class="name" style="color:grey;">`+Pname+`</div>
+                    <div class="price"><span class="verysmall">₹`+Price(jup)+`</span></div>
+                </div>
+            </a>`;
+        }
+        else{
+            newA = `<a href="/product.html?name=`+Pname+"&jup="+jup+`" class="cardself">
+                <img src="/products/`+jup+`/`+viewImg+`" alt="" class="cardIMG">
+                <div class="contentCard">
+                    <div class="name" style="color:grey;">`+Pname+`</div>
+                    <div class="price"><span class="verysmall">₹`+Price(jup)+`</span></div>
+                </div>
+            </a>`;
+        }
+        car1.innerHTML += newA
+    }
+}
---
+++ javascript/data.js
@@ -0,0 +1,181 @@
+allData = {
+    'JUP 01': {
+        'Name': 'wallet', 'Catagory': ['Mens'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'],
+        'Price': {'default':680}
+    },
+    'JUP 02': { 'Name': 'Phone cover', 'Catagory': ['phone cover'], 'Media': ['1.jpg', '2.jpg', '3.jpg'], 'Price': {'default':200} },
+    'JUP 03': { 'Name': 'Rainbow steel Water Bottle', 'Catagory': ['Bottle'], 'Media': ['1.jpg'], 'Price': {'default':540} },
+    'JUP 04': { 'Name': 'kada', 'Catagory': ['Men'], 'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.mp4'], 'Price': {'default':350} },
+    'JUP 05': {
+        'Name': 'Steel Water Bottles', 'Catagory': ['Bottle'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'],
+        'Price': {'default':550}
+    },
+    'JUP 06': { 'Name': 'Apple Cut Pen Stand', 'Catagory': ['Gift'], 'Media': ['1.jpg'], 'Price': {'default':550} },
+    'JUP 07': {
+        'Name': 'Temperature Rainbow Flask', 'Catagory': ['Gift'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'],
+        'Price': {'default':650}
+    },
+    'JUP 08': { 'Name': 'Hot Cold Flask', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':760} },
+    'JUP 09': { 'Name': 'Metal Pen', 'Catagory': ['Gift'], 'Media': ['1.jpg'], 'Price': {'default':200} },
+    'JUP 10': { 'Name': 'Vintage Watches', 'Catagory': ['Watches'], 'Media': ['1.jpg', '2.mp4'], 'Price': {'default':450} },
+    'JUP 11': { 'Name': 'Mini Album', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.mp4'], 'Price': {'default':450} },
+    'JUP 12': { 'Name': 'Customized Pen Stand', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':650} },
+    'JUP 13': { 'Name': 'Couple Bottle Set', 'Catagory': ['Couple'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':1100} },
+    'JUP 14': { 'Name': 'Metal Strip wallet', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.jpg', '3.jpg'], 'Price': {'default':650} },
+    'JUP 15': { 'Name': 'Leather Ladies Wallet', 'Catagory': ['Ladies'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':560} },
+    'JUP 16': { 'Name': 'Dancing Cactus', 'Catagory': ['Toy'], 'Media': ['1.mp4', '2.mp4', '3.mp4'], 'Price': {'default':1000} },
+    'JUP 17': { 'Name': 'Temperature steel flask', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':1180} },
+    'JUP 18': { 'Name': 'Rainbow Thermo Bottle', 'Catagory': ['Bottle'], 'Media': ['1.jpg', '2.jpg', '3.jpg'], 'Price': {'default':960} },
+    'JUP 19': { 'Name': 'Steel Air  Tight Lunch Box', 'Catagory': ['Kids'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':1000} },
+    'JUP 20': { 'Name': 'customized rotating shadow box', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.mp4', '3.mp4'], 'Price': {'default':1100} },
+    'JUP 21': { 'Name': 'customized Hexa Revolving Lamp', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.jpg', '3.mp4'], 'Price': {'default':1100} },
+    'JUP 22': { 'Name': 'Cubical Revolving lamp with bightest led', 'Catagory': ['Lamps'], 'Media': ['1.mp4'], 'Price': {'default':700} },
+    'JUP 23': {
+        'Name': 'hot and cold couple combo flask', 'Catagory': ['Couple'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+        'Price': {'default':1450}
+    },
+    'JUP 24': { 'Name': 'Customized LED frame', 'Catagory': ['Frame'], 'Media': ['1.jpg', '2.mp4'], 'Price': {'default':960} },
+    'JUP 25': { 'Name': 'Customized luggage tag', 'Catagory': ['Traveling'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':300} },
+    'JUP 26': { 'Name': 'LED Photo clips', 'Catagory': ['gift'], 'Media': ['1.jpg', '2.jpg', '3.jpg'], 'Price': {'default':540} },
+    'JUP 27': { 'Name': '7in 1 mens combo', 'Catagory': ['Men'], 'Media': ['1.mp4'], 'Price': {'default':2240} },
+    'JUP 28': { 'Name': 'kitkat combo', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.mp4'], 'Price': {'default':860} },
+    'JUP 29': {
+        'Name': 'Pen and wallet combo', 'Catagory': ['Gift'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'],
+        'Price': {'default':599}
+    },
+    'JUP 30': { 'Name': 'customized magic mug', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.jpg', '3.mp4', '4.mp4'], 'Price': {'default':350} },
+    'JUP 31': { 'Name': 'Unisex zip wallet', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.mp4'], 'Price': {'default':550} },
+    'JUP 32': { 'Name': 'Customized rotating shadow Box', 'Catagory': ['Gift'], 'Media': ['1.mp4'], 'Price': {'default':900} },
+    'JUP 33': {
+        'Name': 'Special caricature', 'Catagory': ['Caricature'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.mp4',
+            'WhatsApp Video 2022-09-08 at 6.45.05 PM.mp4'],
+        'Price': {'default':380}
+    },
+    'JUP 34': { 'Name': 'Customized calender theme mug', 'Catagory': ['Gift'], 'Media': [], 'Price': {'default':200} },
+    'JUP 35': {
+        'Name': 'Customized LED keychain and led pen combo', 'Catagory': ['Gift'],
+        'Media': ['1.jpg', '2.mp4'],
+        'Price': {'default':350}
+    }, 'JUP 36': { 'Name': '', 'Catagory': [], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':960} },
+    'JUP 37': {
+        'Name': 'MDF frame with 3 layer', 'Catagory': ['Frame'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.mp4'],
+        'Price': {'default':199}
+    },
+    'JUP 38': { 'Name': 'Mobile photo stand', 'Catagory': ['Gift'], 'Media': ['1.mp4'], 'Price': {'default':760} },
+    'JUP 39': { 'Name': 'Customized wallet pen and keychain combo', 'Catagory': ['Keychain'], 'Media': ['1.mp4'], 'Price': {'default':1400} },
+    'JUP 40': {
+        'Name': 'Rotating lamp with 21 pics', 'Catagory': ['Lamp'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+        'Price': {'default':760}
+    },
+    'JUP 41': {
+        'Name': 'Customized wallet pen and keychain combo', 'Catagory': ['gift'],
+        'Media': ['1.mp4', '2.mp4'],
+        'Price': {'default':160}
+    }, 'JUP 42': {
+        'Name': 'customized LED keychain', 'Catagory': ['Gift'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg',
+            '6.jpg', '7.jpg'],
+        'Price': {'default':760}
+    },
+    'JUP 43': { 'Name': 'wallet + pen combo', 'Catagory': ['Gift'], 'Media': ['1.jpg', '2.jpg', '3.jpg'], 'Price': {'default':460} },
+    'JUP 44': { 'Name': 'steel Bottle', 'Catagory': ['Bottle'], 'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'], 'Price': {'default':520} },
+    'JUP 45': { 'Name': 'customized steel bottle', 'Catagory': ['Bottle'], 'Media': ['1.mp4', '2.jpg'], 'Price': {'default':499} },
+    'JUP 46': { 'Name': 'Customized alarm clock', 'Catagory': ['Clock'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':350} },
+    'JUP 47': {
+        'Name': 'card holder now with premium pen', 'Catagory': ['Gift'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+        'Price': {'default':399}
+    },
+    'JUP 48': {
+        'Name': 'leather keychain', 'Catagory': ['Keychain'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'],
+        'Price': {'default':199}
+    },
+    'JUP 49': {
+        'Name': 'customized pen and keychain', 'Catagory': ['Keychain'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+        'Price': {'default':360}
+    },
+    'JUP 50': {
+        'Name': 'matt steel sipper bottle', 'Catagory': ['Bottle'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+        'Price': {'default':480}
+    },
+    'JUP 51': { 'Name': 'Customized electric lighter', 'Catagory': ['Lighter'], 'Media': ['1.jpg', '2.mp4', '3.mp4'], 'Price': {'default':570} },
+    'JUP 52': {
+        'Name': 'customized premium sling bag', 'Catagory': ['Bag'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'],
+        'Price': {'default':780}
+    },
+    'JUP 53': { 'Name': 'round magic mirror', 'Catagory': ['Mirror'], 'Media': ['1.jpg', '2.mp4'], 'Price': {'default':660} },
+    'JUP 54': { 'Name': 'Customized magic speaker', 'Catagory': ['speaker'], 'Media': ['2.jpg', '2.mp4'], 'Price': {'default':599} },
+    'JUP 55': { 'Name': 'temperature indicator bottle ', 'Catagory': ['Bottle'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':550} },
+    'JUP 56': {
+        'Name': 'Mens daily essential combo', 'Catagory': ['Men'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'],
+        'Price': {'default':1640}
+    },
+    'JUP 57': { 'Name': 'High quality digital print with mat MFD frame', 'Catagory': ['Gift'], 'Media': ['1.jpg'], 'Price': {'default':399} },
+    'JUP 58': {
+        'Name': 'Pen and wallet combo', 'Catagory': ['Men'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'],
+        'Price': {'default':599}
+    },
+    'JUP 59': { 'Name': 'CROCODILE wallet', 'Catagory': ['Men'], 'Media': ['1.jpg', '2.jpg'], 'Price': {'default':599} },
+    'JUP 60': { 'Name': 'Customized magic cushion', 'Catagory': ['Cushion'], 'Media': ['1.mp4', '2.mp4', '3.mp4'], 'Price': {'default':599} },
+    'JUP 61': {
+        'Name': 'Classic musical led box', 'Catagory': ['Musical'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.mp4'],
+        'Price': {'default':640}
+    },
+    'JUP 62': {
+        'Name': 'customized pen and keychain combo', 'Catagory': ['Keychain'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+        'Price': {'default':360}
+    },
+    'JUP 63': {
+        'Name': ' High quality digital print with mat MFD frame', 'Catagory': ['gift'],
+        'Media': ['1.jpg', '2.jpg'],
+        'Price': {'default':340}
+    },
+    'JUP 64': {
+        'Name': 'High quality digital print with mat MFD frame', 'Catagory': ['Gift'],
+        'Media': ['1.jpg', '2.jpg'],
+        'Price': {'default':440}
+    },
+    'JUP 65': {
+        'Name': 'High quality digital print with mat MFD frame', 'Catagory': ['Gift'],
+        'Media': ['1.jpg', '2.jpg'],
+        'Price': {'default':540}
+    }, 
+    'JUP 66': {
+        'Name': 'Leather eyewear case', 'Catagory': ['Case'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'],
+        'Price': {'default':440}
+    },
+    'JUP 67': {
+        'Name': 'Faux leather ladies wallet 2.0 ', 'Catagory': ['Ladies'],
+        'Media': ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+        'Price': {'default':560}
+    },
+    'JUP 68': { 'Name': 'Kids lunch box', 'Catagory': ['Kids'], 'Media': ['1.jpg', '2.jpg', '3.jpg'], 'Price': {'default':899} }
+}
+
+
+function name(jup){
+    return allData[jup]['Name'];
+}
+
+
+function Price(jup){
+    ps = allData[jup]['Price'];
+    return ps['default'];
+}
